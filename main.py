18685e3a1d518f3c573ad185f4fac4ef991156ae
adiithyashah <adiithyashah@gmail.com>
2026-04-18 21:05:32 -0400

    Initial project setup

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print ("Hello Adithya")
+import requests
+
+url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+response = requests.get(url)
+data = response,json()
+
+print(data)
